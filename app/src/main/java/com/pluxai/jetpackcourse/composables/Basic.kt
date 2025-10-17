9b3c03faf176d0a3d9ce2524ea74ec9793846e5c
Vishal Chand <sam.chand96@gmail.com>
2025-10-17 20:14:33 +0530

    Learned about the creation of the composable functions.

---
+++ app/src/main/java/com/pluxai/jetpackcourse/composables/Basic.kt
@@ -0,0 +1,11 @@
+package com.pluxai.jetpackcourse.composables
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Greeting(text: String) {
+    Text(
+        text = text
+    )
+}
